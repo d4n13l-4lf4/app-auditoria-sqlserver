2bffa7a792939895e91bd2bef173f13e9863634f
Daniel Aucatoma <danielaucatoma1996@hotmail.com>
2019-01-20 10:52:25 -0500

    Creación del servicio de log y generación de scripts para chequeo de anomalías de datos.

---
+++ src/app/sql_queries/anomalias-datos.ts
@@ -0,0 +1,5 @@
+export const anomalias_constraints = `DBCC CHECKCONSTRAINTS WITH ALL_CONSTRAINTS`;
+export const anomalias_relaciones_necesarias =
+  (tabla_padre, columna_padre, tabla_hija, columna_hija ) => `
+    SELECT * FROM ${tabla_hija} WHERE ${columna_hija} NOT IN (SELECT ${columna_padre} from ${tabla_padre})
+  `;
